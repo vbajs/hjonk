4555df1f6721e05195af6fb7f53294f2d133927e
Yahya Wessam <yahyawessam2002@gmail.com>
2022-08-16 02:17:37 +0200

    oldQuote: Fix many broken stuffs
    Fix where context menu label would be blank
    Fix where quoted text could not be inserted into textbox (thanks Ducko!)
    Fix where settings where not saved
    Bump verison to 1.0.1fix
    Cleanup code, no longer use Cyno's mannerism and removed depreacted
    onLoadingFinished

--- oldQuote/index.js
+++ oldQuote/index.js
@@ -1,68 +1,69 @@
-import * as webpackModules from "@goosemod/webpack";
-import {contextMenu} from "@goosemod/patcher";
-import {createItem, removeItem} from "@goosemod/settings";
-import {version} from "./goosemodModule.json";
+import * as webpackModules from '@goosemod/webpack';
+import { contextMenu } from '@goosemod/patcher';
+import { createItem, removeItem } from '@goosemod/settings';
+import { version } from './goosemodModule.json';
 
-let mentionAuthor;
+let settings = {
+  'mentionAuthor': false,
+}
 
 let unpatch;
 
 export default {
   goosemodHandlers: {
-    onImport: () => {
-      const {ComponentDispatch} =
-        webpackModules.findByProps("ComponentDispatch");
-      const {ComponentActions} = webpackModules.findByProps("ComponentActions");
-      const {Messages} = webpackModules.findByProps("Messages");
 
-      unpatch = contextMenu.patch("message", {
-        label: Messages.QUOTE,
+    onImport: () => {
+      const { ComponentDispatch } =
+        webpackModules.findByProps('ComponentDispatch');
+      const { ComponentActions } = webpackModules.findByProps('ComponentActions');
+
+      unpatch = contextMenu.patch('message', {
+        label: 'Quote Message',
         action: (_, props) => {
-          const lines = props.message.content.split("\n");
+          const lines = props.message.content.split('\n');
           for (let i = 0; i < lines.length; i++) {
-            lines[i] = "> " + lines[i];
+            lines[i] = '> ' + lines[i];
           }
 
           const author = `<@${props.message.author.id}> `;
 
-          const out = lines.join("\n") + "\n";
+          const out = lines.join('\n') + '\n';
 
           ComponentDispatch.dispatchToLastSubscribed(
             ComponentActions.INSERT_TEXT,
             {
-              content: out + (mentionAuthor ? author : ""),
+              plainText: out + (settings.mentionAuthor ? author : ''),
             }
           );
         },
       });
-    },
 
-    onLoadingFinished: () => {
-      createItem("Old Quote", [
+      createItem('Old Quote', [
         version,
 
         {
-          type: "header",
-          text: "Toggle the following features as per your liking",
+          type: 'header',
+          text: 'Toggle the following features as per your liking',
         },
         {
-          type: "toggle",
-          text: "Mention the author",
+          type: 'toggle',
+          text: 'Mention the author',
           subtext:
             "Adds mention of the author of the message you're quoting to",
           onToggle: (c) => {
-            return (mentionAuthor = c);
+            return (settings.mentionAuthor = c);
           },
-          isToggled: () => {
-            return mentionAuthor;
-          },
+          isToggled: () => settings.mentionAuthor,
         },
       ]);
     },
 
+    getSettings: () => [settings],
+    loadSettings: ([_settings]) => {settings = _settings},
+
     onRemove: () => {
       unpatch();
-      removeItem("Old Quote");
+      removeItem('Old Quote');
     },
   },
 };
